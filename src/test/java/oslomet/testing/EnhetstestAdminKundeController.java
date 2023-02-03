deb282ac1eba32e0a41644fde6997c450495ee2f
YlvaEvenrud <s364748@oslomet.no>
2023-02-03 11:43:29 +0100

    Endret lagre og slett

--- src/test/java/oslomet/testing/EnhetstestAdminKundeController.java
+++ src/test/java/oslomet/testing/EnhetstestAdminKundeController.java
@@ -3,6 +3,7 @@ package oslomet.testing;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import oslomet.testing.API.AdminKundeController;
 import oslomet.testing.DAL.AdminRepository;
 import oslomet.testing.Models.Kunde;
@@ -18,7 +19,7 @@ import static org.mockito.Mockito.when;
 
 public class EnhetstestAdminKundeController {
     @InjectMocks
-    AdminKundeController adminKundeController;
+    private AdminKundeController adminKundeController;
 
     @Mock
     private AdminRepository repository;
@@ -45,6 +46,8 @@ public class EnhetstestAdminKundeController {
 
         //Mockito.when(sjekk.loggetInn()).thenReturn("OK");
         //Mockito.when(repository.lagreKunde((any(Kunde.class)))).thenReturn("OK");
+        //Mockito.when(repository.registrerKunde((any(Kunde.class)))).thenReturn("OK");
+       when(sjekk.loggetInn()).thenReturn("OK");
 
         // act
         String resultat = adminKundeController.lagreKunde(kunde1);
@@ -53,4 +56,41 @@ public class EnhetstestAdminKundeController {
         assertEquals("OK",resultat);
 
     }
+    @Test
+    public void lagre_ikkeLoggetInn(){
+        // arrange
+        Kunde kunde1 = new Kunde("01010110523","Lene", "Jensen", "Askerveien 22", "3270", "Asker", "22224444", "HeiHei");
+
+        // act
+        String resultat = adminKundeController.lagreKunde(kunde1);
+
+        // assert
+        assertNull(resultat);
+    }
+
+    @Test
+    public void slett_loggetInn(){
+        // arrange
+        String personnummer = new String("567676");
+        when(sjekk.loggetInn()).thenReturn("345242");
+
+        // act
+        String resultat = adminKundeController.slett(personnummer);
+
+        // assert
+        assertEquals("OK",resultat);
+
+    }
+    @Test
+    public void slett_ikkeLoggetInn(){
+        // arrange
+        String persnummer = new String("567676");
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        // act
+
+
+        // assult
+       }
+
 }
