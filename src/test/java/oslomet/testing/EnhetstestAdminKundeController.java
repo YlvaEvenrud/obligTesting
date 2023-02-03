b81c7aa3858467831a9370fbbceadf1cc8b9d280
AqualunaIsTaken <andreasdaarstad@gmail.com>
2023-02-03 11:38:15 +0100

    - Lagt til @RunWith(MockitoJUnitRunner.class) i EnhetstestAdminKundeController. - Lagt til endreKunde tester, ikkeLoggetInn passer ikke

--- src/test/java/oslomet/testing/EnhetstestAdminKundeController.java
+++ src/test/java/oslomet/testing/EnhetstestAdminKundeController.java
@@ -1,8 +1,10 @@
 package oslomet.testing;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import oslomet.testing.API.AdminKundeController;
 import oslomet.testing.DAL.AdminRepository;
 import oslomet.testing.Models.Kunde;
@@ -16,6 +18,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class EnhetstestAdminKundeController {
     @InjectMocks
     AdminKundeController adminKundeController;
@@ -39,9 +42,9 @@ public class EnhetstestAdminKundeController {
     }
 
     @Test
-    public void lagre_loggetInn(){
+    public void lagre_loggetInn() {
         // arrange
-        Kunde kunde1 = new Kunde("01010110523","Lene", "Jensen", "Askerveien 22", "3270", "Asker", "22224444", "HeiHei");
+        Kunde kunde1 = new Kunde("01010110523", "Lene", "Jensen", "Askerveien 22", "3270", "Asker", "22224444", "HeiHei");
 
         //Mockito.when(sjekk.loggetInn()).thenReturn("OK");
         //Mockito.when(repository.lagreKunde((any(Kunde.class)))).thenReturn("OK");
@@ -50,7 +53,33 @@ public class EnhetstestAdminKundeController {
         String resultat = adminKundeController.lagreKunde(kunde1);
 
         // assert
-        assertEquals("OK",resultat);
+        assertEquals("OK", resultat);
 
+    }
+
+    @Test
+    public void endreKunde_LoggetInn() {
+        //arrange
+        Kunde kunde1 = new Kunde("01010110523", "Lene", "Jensen", "Askerveien 22", "3270", "Asker", "22224444", "HeiHei");
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+
+        //act
+        String resultat = adminKundeController.endre(kunde1);
+
+        //assert
+        assertNull(resultat);
+    }
+
+    @Test
+    public void endreKunde_IkkeLoggetInn(){
+        // arrange
+        Kunde kunde1 = new Kunde("01010110523", "Lene", "Jensen", "Askerveien 22", "3270", "22224444", "HeiHei");
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        // act
+        String resultat = adminKundeController.endre(kunde1);
+
+        // assert
+        assertNull(resultat);
     }
 }
