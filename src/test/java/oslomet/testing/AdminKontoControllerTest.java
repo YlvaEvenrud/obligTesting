370c285a10541c20e68f05c4519ad02897f8d4bc
SiriBJ <89572427+SiriBJ@users.noreply.github.com>
2023-02-05 13:53:39 +0100

    Gjort ferdig SlettKonto testene

--- src/test/java/oslomet/testing/AdminKontoControllerTest.java
+++ src/test/java/oslomet/testing/AdminKontoControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.ArgumentMatchers.anyString;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
 import oslomet.testing.API.AdminKontoController;
@@ -76,5 +78,33 @@ public class AdminKontoControllerTest {
 
         // assert
         assertEquals("Ikke innlogget",resultat);
+    }
+
+    @Test
+    public void slettKonto_LoggetInn(){
+        //arrange
+
+        when(sjekk.loggetInn()).thenReturn("01010110523");
+        when(repository.slettKonto(anyString())).thenReturn("OK");
+
+        //act
+
+        String resultat = adminKontoController.slettKonto("105010123456");
+
+        //assert
+
+        assertEquals("OK", resultat);
+    }
+
+    @Test
+    public void slettKonto_IkkeLoggetInn(){
+        //arrange
+        when(sjekk.loggetInn()).thenReturn(null);
+
+        //act
+        String resultat = adminKontoController.slettKonto("105010123456");
+
+        //assert
+        assertEquals("Ikke innlogget", resultat);
     }
 }
